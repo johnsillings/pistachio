7f53a1eff7a75c15e3e1738717407bf7bbd80fa6
John Sillings <johnsillings@Johns-iMac.fios-router.home>
2019-08-24 19:55:16 -0400

    initial commit with core tracking files from tracker

---
+++ animations.rb
@@ -0,0 +1,66 @@
+class Animation
+	def startup_animation
+		print "LET'S"
+		sleep 0.3
+		print "\r"
+		print "LET'S START"
+		sleep 0.3
+		print "\r"
+		print "LET'S START TRACKING"
+		sleep 0.3
+		print "\r"
+		print "LET'S START TRACKING SOME"
+		sleep 0.3
+		print "\r"
+		print "LET'S START TRACKING SOME TIME"
+		sleep 0.3
+		print "\r"
+		print "LET'S START TRACKING SOME TIME MOTHER"
+		sleep 0.3
+		print "\r"
+		print "LET'S START TRACKING SOME TIME MOTHERFUCKER"
+		sleep 1
+		print "\n"
+	end
+
+	def shutdown_animation
+		print "G"
+		sleep 0.1
+		print "\r"
+		print "GO"
+		sleep 0.1
+		print "\r"
+		print "GOO"
+		sleep 0.1
+		print "\r"
+		print "GOOD"
+		sleep 0.1
+		print "\r"
+		print "GOODB"
+		sleep 0.1
+		print "\r"
+		print "GOODBY"
+		sleep 0.1
+		print "\r"
+		print "GOODBYE"
+		sleep 0.1
+		print "\r"
+		print "GOODBYE."
+		sleep 1
+		print "\n"
+	end
+
+		def test_animation
+		print "T"
+		sleep 0.1
+		print "\r"
+		print "TE"
+		sleep 0.1
+		print "\r"
+		print "TES"
+		sleep 0.1
+		print "\r"
+		print "TEST"
+		print "\n"
+	end
+end
